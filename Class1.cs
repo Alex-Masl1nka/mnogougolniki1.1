6c977a500df491034fdd8b8950fe50128233dbf1
Alex-Masl1nka <65232033+Alex-Masl1nka@users.noreply.github.com>
2020-10-22 11:05:57 +0300

    1

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace многоугольники
+{
+    public abstract class Draw
+    {
+        protected int x, y;
+        public Draw(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public abstract void DrawFigure(Graphics graf);
+    }
+    public class circle : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public circle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillEllipse(br, x - 5, y + 5, 10, 10);
+        }
+    }
+    public class square : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public square(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            graf.FillRectangle(br, x - 5, y + 5, 10, 10);
+        }
+    }
+    public class triangle : Draw
+    {
+        SolidBrush br = new SolidBrush(Color.Green);
+        public triangle(int x, int y) : base(x, y)
+        {
+        }
+        public override void DrawFigure(Graphics graf)
+        {
+            //graf.FillRectangle(br, x - 5, y + 5, 10, 10);
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace многоугольники
+{
+    public partial class Form1 : Form
+
+    {
+        Draw[] body = new Draw[3];
+        
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Окружность");
+            comboBox1.Items.Add("Квадрат");
+            comboBox1.Items.Add("Треугольник");
+        }
+        void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            body[0] = new circle(e.X, e.Y);
+            body[1] = new square(e.X, e.Y);
+            body[3] = new triangle(e.X, e.Y);
+            i = ((ComboBox)sender).SelectedIndex;
+            this.Invalidate();
+        }
+    }
+}
